8530cd0f8d8feb76200472b97c6a36b02c4c2708
bahamut555 <ghdfo97@naver.com>
2024-12-21 14:04:26 +0900

    수정4

--- src/fbase.js
+++ src/fbase.js
@@ -1,12 +1,13 @@
 // Import the functions you need from the SDKs you need
-/*import { initializeApp } from "firebase/app"*/
 import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import 'firebase/compat/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
@@ -16,3 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
